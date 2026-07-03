1fcd60c069b3e0379856af6ff04fae5fd2259ab3
Samuel Léobon <samuel.leobon@bworkshop.fr>
2026-07-03 14:44:18 +0200

    combined attribute and options in a single class

--- src/attribute.ts
+++ src/attribute.ts
@@ -17,3 +17,44 @@ export type Attribute = {
   options: string[];
   rules: Rule[];
 };
+
+export type Option = { value: string | number | boolean; weight?: number; pools?: string[] };
+
+export type AttributeOptions = {
+  attribute: Attribute;
+  options: Record<string, Option[]>;
+};
+
+export class AttributeWithOptions {
+  attribute: Attribute;
+  options: Option[]; // all options stored as flat list, options coming from specifically names JSON file (ie. not "default") will have the specific name as bonus pool
+
+  constructor(attribute: Attribute, options: Option[]) {
+    this.attribute = attribute;
+    this.options = options || []; // TODO: automatically populate option groups from JSON files based on parent attribute name
+  }
+
+  get() {
+    return {
+      attribute: this.attribute,
+      options: this.options,
+    };
+  }
+
+  extractPool(pool: string): Option[] {
+    let extractedPool: Option[] = [];
+
+    extractedPool.push(...this.options.filter((option) => option.pools?.includes(pool)));
+
+    return extractedPool;
+  }
+
+  getOption(value: string | number | boolean) {
+    return this.options.find((option) => option.value == value);
+  }
+
+  addOption(option: Option) {
+    if (!this.getOption(option.value)) this.options.push(option);
+    else console.log("This value already exists in the options.");
+  }
+}
